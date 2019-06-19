83a2eded740996b252c8a3f150647a84b93996ef
JoeyDeol <gurjothdeol@gmail.com>
2019-06-19 11:36:21 -0400

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 module.exports = {
   mode: 'development',
 
