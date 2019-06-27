b5459cf3446658cce0c0ead264e2720b010ac5ba
Geoff Coutts <geoff.coutts@gmail.com>
2019-06-27 16:28:29 -0400

    Pass function from box component to children

--- packages/web-components/@sfx/example/src/index.ts
+++ packages/web-components/@sfx/example/src/index.ts
@@ -6,15 +6,25 @@ import './item';
 export class ExampleBestBox extends LitElement { // BaseComponent will replace LitElement
   @property({ type: Array }) items = [];
 
+  testFunc(e) {
+    console.log(this)
+    window.alert(this)
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
   render() {
     return html`
       ${
         this.items.map(item =>
           html`<example-best-box-item
-            name="${item.name}" 
-            price="${item.price}" 
+            name="${item.name}"
+            price="${item.price}"
             rating="${item.rating}"
-            .dimensions="${item.dimensions}" 
+            .dimensions="${item.dimensions}"
+            .onStuff="${this.testFunc}"
           ></example-best-box-item>`
         )
       }
--- packages/web-components/@sfx/example/src/item.ts
+++ packages/web-components/@sfx/example/src/item.ts
@@ -2,24 +2,28 @@ import { LitElement, customElement, html, property } from 'lit-element';
 
 @customElement('example-best-box-item')
 export class ExampleBestBoxItem extends LitElement { // BaseComponent will replace LitElement
-  @property({ type: String, reflect: true }) 
+  @property({ type: String, reflect: true })
   name = '';
-  @property({ type: Number, reflect: true }) 
+  @property({ type: Number, reflect: true })
   rating = '';
-  @property({ type: Number, reflect: true }) 
+  @property({ type: Number, reflect: true })
   price = '';
-  @property({ type: Object }) 
+  @property({ type: Object })
   dimensions = {};
-  @property({ type: Function }) 
+  @property({ type: Function })
   onStuff = () => {};
+
+  createRenderRoot() {
+    return this;
+  }
 
   render() {
     return html`
       <h2>TEST</h2>
-      <p>${ this.name }</p>
-      <p>${ this.rating }</p>
-      <p>${ this.price }</p>
-      <p>${ this.dimensions }</p>
+      <p @click="${this.onStuff}">${this.name}</p>
+      <p>${this.rating}</p>
+      <p>${this.price}</p>
+      <p>${this.dimensions}</p>
     `;
   }
 }
