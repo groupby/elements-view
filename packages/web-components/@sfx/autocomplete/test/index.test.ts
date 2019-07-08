975bf91bfa9d3f5f6a5c4600b21297653d94a3f1
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-07-08 15:29:27 -0400

    SFX-152 Complete majority of autocomplete unit tests

--- packages/web-components/@sfx/autocomplete/test/index.test.ts
+++ packages/web-components/@sfx/autocomplete/test/index.test.ts
@@ -1,53 +1,64 @@
-import { expect, spy, stub } from './utils';
-import { Autocomplete } from '../src/index';
-
-describe('Autcomplete Component', () => {
-  let autocomplete: any = {};
-  beforeEach(() => {
-    autocomplete = new Autocomplete();
-  })
-
-  it('should have property results', () => {
-    expect(autocomplete).to.have.property('results');
-  });
-
-  it('should expose methods', () => {
-    const methods = [
-      'receivedResults'
-    ];
-    methods.forEach((method) => {
-      it(`should expose: ${method}()`, () => {
-        expect(autocomplete[method]).to.be.a('function');
-      });
+  import { expect, spy, stub } from './utils';
+  import { Autocomplete } from '../src/index';
+  
+  describe('Autcomplete Component', () => {
+    let autocomplete: any = {};
+    beforeEach(() => {
+      autocomplete = new Autocomplete();
     })
+  
+    it('should have property results', () => {
+      expect(autocomplete).to.have.property('results');
+    });
+  
+    it('should expose methods', () => {
+      const methods = [
+        'receivedResults'
+      ];
+      methods.forEach((method) => {
+        it(`should expose: ${method}()`, () => {
+          expect(autocomplete[method]).to.be.a('function');
+        });
+      })
+    })
+  
+    it('should call methods in response to events', () => {
+      let receivedResultsStub = stub(autocomplete, 'receivedResults')
+      window.addEventListener('autocompleteDataReceivedEvent', receivedResultsStub)
+      const autocompleteDataReceivedEvent = new CustomEvent('autocompleteDataReceivedEvent', {
+        detail: [{ "title": "Brands", "items": [{ "label": "Cats" }, { "label": "Dogs" }] }, { "title": "default", "items": [{ "label": "Cars" }, { "label": "Bikes" }] }],
+        bubbles: true
+      });
+      window.dispatchEvent(autocompleteDataReceivedEvent);
+      expect(receivedResultsStub.called).to.equal(true);
+      receivedResultsStub.restore();
+    })
+  
+  
+    describe('HTML', () => {
+      it('should call a render function', () => {
+        const renderStub = stub(autocomplete, 'render');
+        autocomplete.render()
+        expect(renderStub.called).to.equal(true);
+      });
+  
+      it('should render the sfx-list element', () => {
+        autocomplete.render()
+        expect()
+      })
+    })
+  
+  
+    describe('receivedResults', () => {
+      it('should update the results property in response to data received', () => {
+        window.addEventListener('autocompleteDataReceivedEvent', autocomplete.receivedResults)
+        const autocompleteDataReceivedEvent = new CustomEvent('autocompleteDataReceivedEvent', {
+          detail: [{ "title": "Brands", "items": [{ "label": "Cats" }, { "label": "Dogs" }] }, { "title": "default", "items": [{ "label": "Cars" }, { "label": "Bikes" }] }],
+          bubbles: true
+        });
+        window.dispatchEvent(autocompleteDataReceivedEvent);
+        expect(autocomplete).to.have.deep.property('results', [{ "title": "Brands", "items": [{ "label": "Cats" }, { "label": "Dogs" }] }, { "title": "default", "items": [{ "label": "Cars" }, { "label": "Bikes" }] }])
+      })
+    })
+  
   })
-  it('should call methods in response to events', () => {
-    console.log('in listen for events')
-    // const autocompleteDataReceivedEvent = new CustomEvent('autocomplete_received_results', { detail: [{"title":"Brands","items":[{"label":"Cats"},{"label":"Dogs"}]},{"title":"default","items":[{"label":"Cars"},{"label":"Bikes"}]}],
-    // bubbles: true });
-    let receivedResultsStub = stub(autocomplete, 'receivedResults');
-    let dispatchEvent = stub(window, 'dispatchEvent');
-    dispatchEvent('autocompleteDataReceivedEvent', { detail: [{"title":"Brands","items":[{"label":"Cats"},{"label":"Dogs"}]},{"title":"default","items":[{"label":"Cars"},{"label":"Bikes"}]}])
-    expect(receivedResultsStub.called).to.equal(true);
-  })
-
-  // describe('recievedResults', () => {
-  //   let autocompleteDataReceivedEventStub;
-  //   beforeEach(() => {
-  //     autocompleteDataReceivedEventStub = stub(window, 'autocompleteDataReceivedEvent').returns(false);
-  //   })
-  //   // let autocomplete: any = {};
-  //   // let autocompleteDataReceivedEventSpy;
-  //   // const autocompleteDataReceivedEvent = new CustomEvent('autocomplete_received_results', { detail: [{"title":"Brands","items":[{"label":"Cats"},{"label":"Dogs"}]},{"title":"default","items":[{"label":"Cars"},{"label":"Bikes"}]}],
-  //   // bubbles: true });
-  //   // beforeEach(() => {
-  //   //   autocomplete = new Autocomplete();
-  //   //   autocompleteDataReceivedEventSpy.spy();
-  //   //   window.('')
-  //   // })
-  //   // it('should get emitted events', () => {
-
-  //   // })
-  //   it('should update results array')
-  })
-
