f62b591f998a8f5f427a31ab0283261e969275c3
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-07-10 11:16:38 -0400

    Add base unit tests for connected and disconnected callbacks

--- packages/web-components/@sfx/base/test/index.test.ts
+++ packages/web-components/@sfx/base/test/index.test.ts
@@ -1,6 +1,7 @@
 import { expect, spy, stub } from './utils';
 import Base from '../src/index';
 import { LitElement } from 'lit-element';
+import { TemplateResult } from 'lit-element';
 
 describe('Base Class', () => {
   let base: any = {};
@@ -31,17 +32,28 @@ describe('Base Class', () => {
     })
   })
 
-  it('should expose methods', () => {
-    const methods = [
-      'addSlots',
-      'firstUpdate',
-      'createRenderRoot'
-    ];
-    methods.forEach((method) => {
-      it(`should expose: ${method}()`, () => {
-        expect(base[method]).to.be.a('function');
-      });
+  describe('connectedCallback', () => {
+    it('should call its super connectedCallback', () => {
+      const litElementConnectedCallbackStub = stub(LitElement.prototype, 'connectedCallback')
+      base.connectedCallback();
+      expect(litElementConnectedCallbackStub).to.have.been.called;
+    })
+  });
+
+  describe('disconnectedCallback', () => {
+    it('should call its super disconnectedCallback', () => {
+      const litElementDisconnectedCallbackStub = stub(LitElement.prototype, 'disconnectedCallback')
+      base.disconnectedCallback();
+      expect(litElementDisconnectedCallbackStub).to.have.been.called;
+    })
+  });
+
+  describe('render function', () => {
+    it('should return an instance of TemplateResult', () => {
+      const result = base.render();
+      expect(result).to.be.an.instanceof(TemplateResult);
     })
   })
+
  
 });
