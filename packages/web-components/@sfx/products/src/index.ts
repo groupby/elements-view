a58b6cd045009e504c9a120fda298ad77df7b445
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-07-11 14:27:47 -0400

    Use mock Product web component

--- packages/web-components/@sfx/products/src/index.ts
+++ packages/web-components/@sfx/products/src/index.ts
@@ -3,6 +3,33 @@
  */
 import { LitElement, customElement, html, property } from 'lit-element';
 
+@customElement('sfx-product')
+class Product extends LitElement {
+  @property({ type: String }) name = ''
+  @property({ type: String }) description = ''
+  @property({ type: Number }) price = ''
+  @property({ type: String }) imageSource = ''
+
+  static get properties() {
+    return {
+      name: { reflect: true },
+      description: { reflect: true },
+      price: { reflect: true },
+      imageSource: { reflect: true },
+    }
+  }
+
+  render() {
+    return html`
+    <div>
+      <img src="${this.imageSource}" />
+      <p>${this.name}: $${this.price}.95</p>
+      <p>${this.description}</p>
+    </div>
+    `
+  }
+}
+
 @customElement('sfx-products')
 export class Products extends LitElement {
   @property({ type: Number }) maxItems = 12;
@@ -13,14 +40,19 @@ export class Products extends LitElement {
       products.push({
         name: `Product ${i + 1}`,
         price: Math.ceil(Math.random() * 10),
+        imageSource: 'https://via.placeholder.com/150',
+        description: 'This product is...'
       })
     }
     return html`
       ${products.map(product => {
         return html`
-          <div>
-            <p>${product.name}: $${product.price}.95</p>
-          </div>
+          <sfx-product
+            name=${product.name}
+            description=${product.description}
+            price=${product.price}
+            imageSource=${product.imageSource}
+            />
         `
       })}
     `;
