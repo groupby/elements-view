70456eb670ff6e6a294ce80edd816c4b03032088
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-07-12 16:44:19 -0400

    Combine delete functionality to keyup event

--- packages/web-components/@sfx/search-box/src/searchbox.ts
+++ packages/web-components/@sfx/search-box/src/searchbox.ts
@@ -40,31 +40,23 @@ export class SearchBox extends LitElement {
     window.dispatchEvent(searchboxxClearedEvent);
   }
 
-  handleKeypress(e) {
-    console.log('in handle keypress e', e)
-    console.log('in handle keypress type of e', typeof e)
-    // console.log((<HTMLInputElement>this.querySelector('#searchInput')).value, 'inputValue')
-    if (e.keyCode === 13) {
-      this.emitSearchEvent();
-    } else {
-      this.searchTerm = (<HTMLInputElement>this.querySelector('#searchInput')).value;
-      this.emitAutocompleteRequestEvent(this.searchTerm)
-    }
-    console.log('this.searchTerm', this.searchTerm)
-  }
-
   updateText(e) {
     this.searchTerm = e.detail;
     let el = (<HTMLInputElement>this.querySelector('#searchInput')).value;
     el = e.detail;
-    console.log('in update text - e', e)
   }
 
   handleKeydown(e) {
     if (e.keyCode === 8) {
       this.searchTerm = this.searchTerm.slice(0, this.searchTerm.length-1);
     }
-    // console.log('this.searchTerm', this.searchTerm)
+    else if (e.keyCode === 13) {
+      this.emitSearchEvent();
+    } else {
+      this.searchTerm = (<HTMLInputElement>this.querySelector('#searchInput')).value;
+      this.emitAutocompleteRequestEvent(this.searchTerm)
+    }
+    console.log('this.searchTerm', this.searchTerm)
   }
 
   clearSearch() {
@@ -75,7 +67,7 @@ export class SearchBox extends LitElement {
 
   render() {
     return html`
-    <input type="text" id="searchInput" placeholder=${this.placeholder} @keyup="${this.handleKeypress}" @keydown="${this.handleKeydown}"></input>
+    <input type="text" id="searchInput" placeholder=${this.placeholder} @keyup="${this.handleKeydown}"></input>
     <button @click=${this.clearSearch}>Clear</button>
     <button @click=${this.emitSearchEvent}>Search</button>
     `;
