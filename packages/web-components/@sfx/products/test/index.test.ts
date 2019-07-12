0145a4121784d4836d2491ae728f3110e1eb4a64
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-07-12 10:50:37 -0400

    Remove dummy test

--- packages/web-components/@sfx/products/test/index.test.ts
+++ packages/web-components/@sfx/products/test/index.test.ts
@@ -1,12 +1,6 @@
 import { expect, spy, stub } from './utils';
 import { Products } from '../src/index';
 
-describe('Dummy test', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
-  });
-});
-
 describe('Products Component ', () => {
   let component: any = {};
   beforeEach(() => {
