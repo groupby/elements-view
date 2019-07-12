33402df4920a224abfa92e6ce4ef5de542ac34f3
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-07-12 16:44:30 -0400

    Update Product tile to take a single attribute

--- packages/web-components/@sfx/products/src/index.ts
+++ packages/web-components/@sfx/products/src/index.ts
@@ -1,23 +1,12 @@
 /**
- * This test comment _supports_ [Markdown and other fun stuff](https://typedoc.org/guides/doccomments/)
+ * The sfx-products web component is designed to wrap and render a number of 
+ * sfx-product components.
  */
 import { LitElement, customElement, css, html, property } from 'lit-element';
 
 @customElement('sfx-product')
 class Product extends LitElement {
-  @property({ type: String }) name = ''
-  @property({ type: String }) description = ''
-  @property({ type: Number }) price = ''
-  @property({ type: String }) imageSource = ''
-
-  static get properties() {
-    return {
-      name: { reflect: true },
-      description: { reflect: true },
-      price: { reflect: true },
-      imageSource: { reflect: true },
-    }
-  }
+  @property({ type: Object }) product: any = {}
 
   render() {
     return html`
@@ -27,9 +16,9 @@ class Product extends LitElement {
         }
       </style>
       <div>
-        <img src="${this.imageSource}" />
-        <p>${this.name}: $${this.price}.95</p>
-        <p>${this.description}</p>
+        <img src="${this.product.imageSource}" />
+        <p>${this.product.name}: $${this.product.price}.95</p>
+        <p>${this.product.description}</p>
       </div>
     `
   }
@@ -80,10 +69,7 @@ export class Products extends LitElement {
       ${this.getRenderableProducts().map(product => {
         return html`
           <sfx-product
-            name="${product.name}"
-            description="${product.description}"
-            price="${product.price}"
-            imageSource="${product.imageSource}"
+            .product="${product}"
             />
         `
       })}
