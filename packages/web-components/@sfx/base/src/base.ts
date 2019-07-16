5c283f5c665d49d7b7c97b43971f69debc9231e9
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-07-16 17:18:45 -0400

    Start documentation comments for base class

--- packages/web-components/@sfx/base/src/base.ts
+++ packages/web-components/@sfx/base/src/base.ts
@@ -3,8 +3,14 @@ import { makeSlot, createChildrenObserver } from './utils';
 
 @customElement('sfx-base')
 export default class Base extends LitElement {
+/**
+ * observer property. Declared, however, not yet assigned a value.
+ */
   observer: MutationObserver;
 
+/**
+ * Creates an instance of the Base class. Are we going to use slots here? should I bother with commenting here?
+ */
   constructor() {
     super();
     this.addSlots();
