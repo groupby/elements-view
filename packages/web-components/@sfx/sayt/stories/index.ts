a173649829faf34d3e3546bbc44934dd063d1687
JoeyDeol <gurjothdeol@gmail.com>
2019-07-19 10:54:06 -0400

    Introduce a way for autocomplete to be populated while in storybook

--- packages/web-components/@sfx/sayt/stories/index.ts
+++ packages/web-components/@sfx/sayt/stories/index.ts
@@ -6,6 +6,23 @@ storiesOf('Components|SAYT', module)
   .addDecorator(withKnobs)
   .add('Default', () => {
     const showAttribute = boolean('show', false) ? 'show' : '';
+    /**
+     * TODO: Need to revisit this and consider a new way to pass in data
+     * into the autocomplete component.
+     * */
+    const autocompleteDataReceivedEvent = new CustomEvent('autocomplete_received_results',
+    {
+      detail: [
+        { "title": "Brands", "items": [{ "label": "Cats" }, { "label": "Dogs" }] },
+        { "title": "default", "items": [{ "label": "Cars" }, { "label": "Bikes" }] }
+      ],
+      bubbles: true
+    });
+    if (showAttribute) {
+      setTimeout(() => {
+        window.dispatchEvent(autocompleteDataReceivedEvent);
+      }, 100);
+    }
 
     return `
       <sfx-sayt
@@ -23,7 +40,7 @@ storiesOf('Components|SAYT', module)
       `
     } 
   })
-  .add('Receiving sayt_hide events and sayt_show ', () => {
+  .add('Responding to Events - sayt_show & sayt_hide ', () => {
     setTimeout(() => {
       window.dispatchEvent(new Event('sfx::sayt_show'));
     }, 3000);
@@ -39,8 +56,8 @@ storiesOf('Components|SAYT', module)
     notes: {
       markdown:`
         # Search As You Type (SAYT)
-        - Receiving sayt_hide event after 3 seconds.
-        - Receiving sayt_show event after 6 seconds.
+        - Receiving sayt_show event after 3 seconds.
+        - Receiving sayt_hide event after 6 seconds.
       `
     }
   })
