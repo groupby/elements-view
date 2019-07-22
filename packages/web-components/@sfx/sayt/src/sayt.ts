c1b431a343621aa4662cf4b1ef49aeb879fa4411
JoeyDeol <gurjothdeol@gmail.com>
2019-07-22 15:33:24 -0400

    Update sayt class documentation

--- packages/web-components/@sfx/sayt/src/sayt.ts
+++ packages/web-components/@sfx/sayt/src/sayt.ts
@@ -3,20 +3,32 @@ import { LitElement, customElement, html, property } from 'lit-element';
 import '@sfx/autocomplete';
 import { SAYT_EVENT } from './events';
 
+/**
+ * The `sfx-sayt` component is responsible for displaying and hiding the
+ * `sfx-autocomplete` and `sfx-products` components.
+ */
 @customElement('sfx-sayt')
 // TODO: look into the Base class so this class can extend it again. The
 // issue that we're seeing is related to changing properties and having
 // the DOM update accurately in response to those property changes.
 export default class Sayt extends LitElement {
+  /**
+   * Determines if the `sfx-autocomplete` component will be hidden or not.
+   */
   @property({ type: Boolean, reflect: true }) hideAutocomplete = false;
+  /**
+   * Determines the visibility of the `sayt` component.
+   */
   @property({ type: Boolean, reflect: true }) show = false;
 
   constructor() {
     super();
-
     this.handleVisibilityEvent = this.handleVisibilityEvent.bind(this);
   }
 
+  /**
+   * Register event listeners.
+   */
   connectedCallback() {
     super.connectedCallback();
 
@@ -24,6 +36,9 @@ export default class Sayt extends LitElement {
     window.addEventListener(SAYT_EVENT.SAYT_HIDE, this.handleVisibilityEvent);
   }
 
+  /**
+   * Remove event listeners.
+   */
   disconnectedCallback() {
     super.disconnectedCallback();
 
@@ -36,12 +51,22 @@ export default class Sayt extends LitElement {
     return this;
   }
 
+  /**
+   * Update component `hidden` property when the `show` property changes.
+   * @param changedProps A map of the all the change properties.
+   */
   updated(changedProps: Map<string, any>) {
     if (changedProps.has('show')) {
       this.hidden = !this.show;
     }
   }
 
+  /**
+   * Method to handle the visibility events associated with the `sayt`
+   * component
+   * @param e The custom event received from sayt-visibility-related
+   * events.
+   */
   handleVisibilityEvent(e: CustomEvent) {
     switch(e.type) {
       case SAYT_EVENT.SAYT_SHOW:
