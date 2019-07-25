eb1e6a45f46d11f09d93c3d4d0d3371377e59ebe
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-07-25 14:14:56 -0400

    WIP: listen for click even to close SAYT

--- packages/web-components/@sfx/sayt/src/sayt.ts
+++ packages/web-components/@sfx/sayt/src/sayt.ts
@@ -30,6 +30,7 @@ export default class Sayt extends LitElement {
 
     window.addEventListener(SAYT_EVENT.SAYT_SHOW, this.showSayt);
     window.addEventListener(SAYT_EVENT.SAYT_HIDE, this.hideSayt);
+    window.addEventListener('click', this.hideSayt);
   }
 
   /**
