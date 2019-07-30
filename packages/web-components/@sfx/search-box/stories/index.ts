e7f5f48d93f587cb3e941a52e17275e621adc36e
Chantal <chandesrochers@gmail.com>
2019-07-30 14:08:08 -0400

    SFX 151 - Search Box Component (#9)
    Created SearchBox component

---
+++ packages/web-components/@sfx/search-box/stories/index.ts
@@ -0,0 +1,56 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import '../src/search-box';
+import { SEARCHBOX_EVENT } from '../src/events';
+
+// start of event dispatch testing
+window.addEventListener(SEARCHBOX_EVENT.SEARCH_REQUEST, e => {
+  console.log('search request received');
+  console.log('event', e);
+});
+
+window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLEAR_CLICK, e => {
+  console.log('search box cleared event received');
+  console.log('event', e);
+});
+
+window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLICK, e => {
+  console.log('search box clicked event received');
+  console.log('event', e);
+});
+
+window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CHANGE, e => {
+  console.log('search box change event received');
+  console.log('event', e);
+});
+// end of event dispatch testing
+
+storiesOf('Components|Searchbox', module)
+  .addDecorator(withKnobs)
+    .add(
+      'Default - with search and clear button',
+      () => `
+    <sfx-search-box searchbutton clearbutton></sfx-search-box>
+  `
+    )
+    .add(
+      'Without any SFX provided buttons',
+      () => `
+    <sfx-search-box></sfx-search-box>
+  `
+    )
+    .add(
+      'Without a clear button, without a search button',
+      () => `
+    <sfx-search-box clearbutton></sfx-search-box>
+  `
+    )
+    .add(
+      'With custom placeholder text',
+      () => `
+    <sfx-search-box placeholder="${text(
+      'Placeholder Title',
+      'Search'
+    )}"></sfx-search-box>
+  `
+    );
