b66c870e20ca96a650a8bc6444db58a2f14bb6b1
ChantalDesrochers <chandesrochers@gmail.com>
2019-08-02 14:59:02 -0400

    Merge products into SAYT functionality

--- packages/web-components/@sfx/sayt/stories/index.ts
+++ packages/web-components/@sfx/sayt/stories/index.ts
@@ -6,27 +6,40 @@ import { getDisplayCode } from '../../../../../.storybook/common';
 
 // @TODO allow for sending event with searchbar ID. This should allow for one
 // story's events to not affect another story.
-const autocompleteDataReceivedEvent = new CustomEvent('sfx::autocomplete_received_results',
-  { detail: [
-      { "title": "Brands", "items": [{ "label": "Cats" }, { "label": "Dogs" }] },
-      { "title": "default", "items": [{ "label": "Cars" }, { "label": "Bikes" }] }
+const autocompleteDataReceivedEvent = new CustomEvent(
+  'sfx::autocomplete_received_results',
+  {
+    detail: [
+      { title: 'Brands', items: [{ label: 'Cats' }, { label: 'Dogs' }] },
+      { title: '', items: [{ label: 'Cars' }, { label: 'Bikes' }] }
     ],
-    bubbles: true }
+    bubbles: true
+  }
 );
 
-function getSayt(searchbar='', showSayt=true): string {
+function getSayt(searchbar = '', showSayt = true): string {
   const showAttribute = boolean('visible', showSayt) ? 'visible' : '';
   const closeText = text('Close link text', 'Close');
-  const showCloseButton = boolean('Show Close button', true) ? 'showclosebutton' : '';
+  const showCloseButton = boolean('Show Close button', true)
+    ? 'showclosebutton'
+    : '';
 
-  return `<sfx-sayt${ searchbar ? ` searchbar="${ searchbar }"` : '' }
-  closetext="${ closeText }"${ showCloseButton ? `
-  ${ showCloseButton }` : ''}${ showAttribute ? `
-  ${ showAttribute }` : ''}
+  return `<sfx-sayt${searchbar ? ` searchbar="${searchbar}"` : ''}
+  closetext="${closeText}"${
+    showCloseButton
+      ? `
+  ${showCloseButton}`
+      : ''
+  }${
+    showAttribute
+      ? `
+  ${showAttribute}`
+      : ''
+  }
 ></sfx-sayt>`;
 }
 
-function emitEventInFuture(event, timeout=100) {
+function emitEventInFuture(event, timeout = 100) {
   setTimeout(() => {
     window.dispatchEvent(event);
   }, timeout);
@@ -34,91 +47,107 @@ function emitEventInFuture(event, timeout=100) {
 
 storiesOf('Components|SAYT', module)
   .addDecorator(withKnobs)
-  .add('Default', () => {
-    emitEventInFuture(autocompleteDataReceivedEvent, 100);
+  .add(
+    'Default',
+    () => {
+      emitEventInFuture(autocompleteDataReceivedEvent, 1000);
 
-    const sayt = getSayt();
-    return `
-      ${ sayt }
+      const sayt = getSayt();
+      return `
+      ${sayt}
 
-      ${ getDisplayCode(sayt) }
+      ${getDisplayCode(sayt)}
     `;
-  }, {
-    notes: {
-      markdown: `
+    },
+    {
+      notes: {
+        markdown: `
         # Search As You Type (SAYT)
         Hardcoded
         
         Here is the documentation for the SAYT component.
       `
-    } 
-  })
+      }
+    }
+  )
   // @TODO Remove these setTimeouts when opening a new story
-  .add('Responding to Events - sayt_hide & sayt_show ', () => {
-    emitEventInFuture(autocompleteDataReceivedEvent, 100);
-    emitEventInFuture(new Event('sfx::sayt_hide'), 2000);
-    emitEventInFuture(new Event('sfx::sayt_show'), 4000);
+  .add(
+    'Responding to Events - sayt_hide & sayt_show ',
+    () => {
+      emitEventInFuture(autocompleteDataReceivedEvent, 100);
+      emitEventInFuture(new Event('sfx::sayt_hide'), 2000);
+      emitEventInFuture(new Event('sfx::sayt_show'), 4000);
 
-    const sayt = getSayt('', false);
-    return `
-      ${ sayt }
-      ${ getDisplayCode(sayt) }
+      const sayt = getSayt('', false);
+      return `
+      ${sayt}
+      ${getDisplayCode(sayt)}
     `;
-  }, {
-    notes: {
-      markdown:`
+    },
+    {
+      notes: {
+        markdown: `
         # Search As You Type (SAYT)
         - Show automatically once sub-component Autocomplete receives results.
         - Receiving sayt_hide event after 2 seconds.
         - Receiving sayt_show event after 4 seconds.
       `
+      }
     }
-  })
-  .add('SAYT with simple search input', () => {
-    emitEventInFuture(autocompleteDataReceivedEvent, 100);
+  )
+  .add(
+    'SAYT with simple search input',
+    () => {
+      emitEventInFuture(autocompleteDataReceivedEvent, 100);
 
-    const input = `<input type="text" id="search-bar" placeholder="Search here" />`;
-    const sayt = getSayt();
-    return `
-      ${ input }
+      const input = `<input type="text" id="search-bar" placeholder="Search here" />`;
+      const sayt = getSayt();
+      return `
+      ${input}
       <br />
-      ${ sayt }
-      ${ getDisplayCode(`${ input }
-${ sayt }`) }
-    `
-  }, {
-    notes: {
-      markdown:`
+      ${sayt}
+      ${getDisplayCode(`${input}
+${sayt}`)}
+    `;
+    },
+    {
+      notes: {
+        markdown: `
         #Search As You Type (SAYT)
         Demonstrating initially visible SAYT for showing/hiding.
       `
+      }
     }
-  })
-  .add('SAYT with multiple search inputs', () => {
-    emitEventInFuture(autocompleteDataReceivedEvent, 100);
+  )
+  .add(
+    'SAYT with multiple search inputs',
+    () => {
+      emitEventInFuture(autocompleteDataReceivedEvent, 100);
 
-    const input1 = `<input type="text" id="search-bar1" placeholder="Search here" />`;
-    const input2 = `<input type="text" id="search-bar2" placeholder="Or search here" />`;
-    const sayt1 = getSayt('search-bar1');
-    const sayt2 = getSayt('search-bar2');
+      const input1 = `<input type="text" id="search-bar1" placeholder="Search here" />`;
+      const input2 = `<input type="text" id="search-bar2" placeholder="Or search here" />`;
+      const sayt1 = getSayt('search-bar1');
+      const sayt2 = getSayt('search-bar2');
 
-    return `${ input1 }<br />
-${ sayt1 }
+      return `${input1}<br />
+${sayt1}
 <hr />
-${ input2 }<br />
-${ sayt2 }
+${input2}<br />
+${sayt2}
 
-${ getDisplayCode(`${ input1 }
-${ sayt1 }
+${getDisplayCode(`${input1}
+${sayt1}
 
-${ input2 }
-${ sayt2 }`) }
+${input2}
+${sayt2}`)}
     `;
-  }, {
-    notes: {
-      markdown:`
+    },
+    {
+      notes: {
+        markdown: `
         #Search As You Type (SAYT)
         Demonstrating multiple SAYT components. This proves that each Search/SAYT pair acts independently.
       `
+      }
     }
-  });
+  );
