1464f3b1ee72d3a7a8979a5172a529a886b5d2b4
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-06 10:54:52 -0400

    Move shared vars and functions to common file

--- packages/web-components/@sfx/sayt/stories/index.ts
+++ packages/web-components/@sfx/sayt/stories/index.ts
@@ -2,49 +2,17 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 import '../src/index.ts';
-import { getDisplayCode } from '../../../../../.storybook/common';
-import { ProductModel } from '../../product/src/product';
-
-const sampleProducts = [
-  {
-    title: 'Best Shoe',
-    price: 39.99,
-    label: 'New Product',
-    promo: '25% off',
-    imageSrc:
-      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
-    imageAlt: 'A spicy red shoe'
-  } as ProductModel,
-  {
-    title: 'Greatest Shoe',
-    price: 49.99,
-    label: 'Classic Product',
-    promo: '25% off',
-    imageSrc:
-      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
-    imageAlt: 'A classic red shoe'
-  } as ProductModel
-];
+import {
+  getDisplayCode,
+  emitEventInFuture,
+  autocompleteDataReceivedEvent,
+  sampleProducts,
+  productsEvent
+} from '../../../../../.storybook/common';
 
 for (let i = 0; i < 12; i++) {
   sampleProducts.push(sampleProducts[i % 2]);
 }
-
-// @TODO allow for sending event with searchbar ID. This should allow for one
-// story's events to not affect another story.
-const autocompleteDataReceivedEvent = new CustomEvent('sfx::autocomplete_received_results', {
-  detail: [
-    { title: 'Brands', items: [{ label: 'Cats' }, { label: 'Dogs' }] },
-    { title: '', items: [{ label: 'Cars' }, { label: 'Bikes' }] }
-  ],
-  bubbles: true
-});
-
-const productsEvent = new CustomEvent('sfx:provide-products', {
-  detail: {
-    products: sampleProducts
-  }
-});
 
 function getSayt(searchbar = '', showSayt = true): string {
   const showAttribute = boolean('visible', showSayt) ? 'visible' : '';
@@ -83,12 +51,6 @@ function getStyle() {
   sfx-product {
   }
 </style>`;
-}
-
-function emitEventInFuture(event, timeout = 100) {
-  setTimeout(() => {
-    window.dispatchEvent(event);
-  }, timeout);
 }
 
 storiesOf('Components|SAYT', module)
