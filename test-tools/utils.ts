f5c0c7b01af231f3c272c4b252a5778d31b03ca0
JoeyDeol <gurjothdeol@gmail.com>
2019-08-06 10:55:02 -0400

    Include typings for waitForUpdateComplete

--- test-tools/utils.ts
+++ test-tools/utils.ts
@@ -1,5 +1,6 @@
 import * as sinonImport from 'sinon';
 import * as chaiImport from 'chai';
+import { LitElement } from 'lit-element';
 
 declare var global: any;
 
@@ -9,6 +10,6 @@ export const expect = chai.expect;
 export const stub = sinon.stub;
 export const spy = sinon.spy;
 
-export function waitForUpdateComplete(node) {
-  return node.updateComplete.then((hasUpdateCompleted) => hasUpdateCompleted || waitForUpdateComplete(node));
+export function waitForUpdateComplete(node: LitElement): Promise<boolean> {
+  return node.updateComplete.then((hasUpdateCompleted: boolean) => hasUpdateCompleted || waitForUpdateComplete(node));
 }
