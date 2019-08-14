15c2b1acc8ffc49dea296dba7490545da78c6426
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-14 14:04:09 -0400

    Change import method for products event in sayt

--- packages/web-components/@sfx/sayt/src/sayt.ts
+++ packages/web-components/@sfx/sayt/src/sayt.ts
@@ -1,5 +1,5 @@
 import { LitElement, customElement, html, property, PropertyValues } from 'lit-element';
-import { PRODUCTS_EVENT } from '@sfx/products';
+import { PRODUCTS_EVENT } from '../../products/src/index';
 import { SAYT_EVENT } from './events';
 import { AUTOCOMPLETE_RECEIVED_RESULTS_EVENT } from '../../autocomplete/src/events';
 
