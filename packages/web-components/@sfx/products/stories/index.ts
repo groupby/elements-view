7e9d75290fbd2161b694321004a8414271b21f5a
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-19 11:28:19 -0400

    Fix products event payload

--- packages/web-components/@sfx/products/stories/index.ts
+++ packages/web-components/@sfx/products/stories/index.ts
@@ -2,10 +2,41 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs } from '@storybook/addon-knobs';
 import { dispatchProvideProductsEvent } from '../../../../../.storybook/common';
 import addons from '@storybook/addons';
+import { ProductModel } from '@sfx/product'
 
 function dispatchRandomProducts() {
   return dispatchProvideProductsEvent(Math.ceil(Math.random() * 6));
 }
+
+const sampleProducts: ProductModel[] = [
+  {
+    title: 'Best Shoe',
+    price: 39.99,
+    label: 'New Product',
+    promo: '25% off',
+    imageSrc:
+      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
+    imageAlt: 'A spicy red shoe'
+  },
+  {
+    title: 'Greatest Shoe',
+    price: 49.99,
+    label: 'Classic Product',
+    promo: '25% off',
+    imageSrc:
+      'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
+    imageAlt: 'A classic blue shoe'
+  }
+];
+
+const productsResultsEvent = [
+  {
+    name: 'sfx::provide_products',
+    payload: {
+      products: sampleProducts
+  },
+}
+];
 
 storiesOf('Components|Products', module)
   .addDecorator(withKnobs)
@@ -17,9 +48,7 @@ storiesOf('Components|Products', module)
     `;
   },
   {
-    customEvents: productsResultsEvent
-  },
-  {
+    customEvents: productsResultsEvent,
     notes: {
       markdown: `
         # Products
@@ -30,17 +59,20 @@ storiesOf('Components|Products', module)
         emitting an event which contains the products to be rendered.
       `
     }
+  },
+  {
+
   })
   .add('Default - event listening', () => {
-    for (let i = 1; i < 6; i++) {
-      setTimeout(() => dispatchRandomProducts(), i * 2000);
-    }
-
-    return `
+    // for (let i = 1; i < 6; i++) {
+    //   setTimeout(() => dispatchRandomProducts(), i * 2000);
+    // }
+      return `
       <sfx-products></sfx-products>
     `;
   },
   {
+    customEvents: productsResultsEvent,
     notes: {
       markdown: `
         # Products - event listening
