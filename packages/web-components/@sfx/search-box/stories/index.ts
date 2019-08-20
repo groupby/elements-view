b994488a3591b961b20991d539c3a648719eb133
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-20 14:39:21 -0400

    Searchbox function working without attributes included

--- packages/web-components/@sfx/search-box/stories/index.ts
+++ packages/web-components/@sfx/search-box/stories/index.ts
@@ -1,68 +1,69 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, text } from '@storybook/addon-knobs';
-import '../src/search-box';
-import { SEARCHBOX_EVENT } from '../src/events';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import '../src';
+// import { SEARCHBOX_EVENT } from '../src/events';
 
 // start of event dispatch testing
-window.addEventListener(SEARCHBOX_EVENT.SEARCH_REQUEST, e => {
-  console.log('search request received');
-  console.log('event', e);
-});
+// window.addEventListener(SEARCHBOX_EVENT.SEARCH_REQUEST, e => {
+//   console.log('search request received');
+//   console.log('event', e);
+// });
 
-window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLEAR_CLICK, e => {
-  console.log('search box cleared event received');
-  console.log('event', e);
-});
+// window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLEAR_CLICK, e => {
+//   console.log('search box cleared event received');
+//   console.log('event', e);
+// });
 
-window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLICK, e => {
-  console.log('search box clicked event received');
-  console.log('event', e);
-});
+// window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CLICK, e => {
+//   console.log('search box clicked event received');
+//   console.log('event', e);
+// });
 
-window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CHANGE, e => {
-  console.log('search box change event received');
-  console.log('event', e);
-});
+// window.addEventListener(SEARCHBOX_EVENT.SEARCHBOX_CHANGE, e => {
+//   console.log('search box change event received');
+//   console.log('event', e);
+// });
 // end of event dispatch testing
+
+function getSearchBoxComponent(): string {
+  // const placeholder = text('Placeholder Text', 'Search Here');
+  // const searchButton = boolean('Search button visible', true) ? 'searchbutton' : '';
+  // const clearButton = boolean('Clear button visible', true) ? 'clearbutton' : '';
+  return '<sfx-search-box></sfx-search-box>'
+  // return '<sfx-search-box\n'
+  //   + ` placeholder="${placeholder}"\n`
+  //   + (searchButton ? ` ${searchButton}\n` : '')
+  //   + (clearButton ? ` ${clearButton}\n` : '')
+  //   + '><sfx-search-box>'
+}
 
 storiesOf('Components|Searchbox', module)
   .addDecorator(withKnobs)
-    .add(
-      'Default - with search and clear button',
-      () => `
-    <sfx-search-box searchbutton clearbutton></sfx-search-box>
-    <button @click="${this.updateTextEvent}">
-      Click to dispatch update search term event
-    </button>
-  `
-    )
-    .add(
-      'Without any SFX provided buttons',
-      () => `
-    <sfx-search-box></sfx-search-box>
-    <button @click="${this.updateTextEvent}">
-      Click to dispatch update search term event
-    </button>
-  `
-    )
-    .add(
-      'With a clear button, without a search button',
-      () => `
-    <sfx-search-box clearbutton></sfx-search-box>
-    <button @click="${this.updateTextEvent}">
-      Click to dispatch update search term event
-    </button>
-  `
-    )
-    .add(
-      'With custom placeholder text',
-      () => `
-    <sfx-search-box placeholder="${text(
-      'Placeholder Title',
-      'Placeholder here...'
-    )}"></sfx-search-box>
-    <button @click="${this.updateTextEvent}">
-      Click to dispatch update search term event
-    </button>
-  `
-    );
+    .add('Default', () => {
+      const searchBoxComponent = getSearchBoxComponent();
+      console.log('searchboxcomponent', searchBoxComponent)
+      return `
+      ${searchBoxComponent}
+      `;
+    })
+  //   .add(
+  //     'Without any SFX provided buttons',
+  //     () => `
+  //   <sfx-search-box></sfx-search-box>
+  // `
+  //   )
+  //   .add(
+  //     'With a clear button, without a search button',
+  //     () => `
+  //   <sfx-search-box clearbutton></sfx-search-box>
+  // `
+  //   )
+  //   .add(
+  //     'With custom placeholder text',
+  //     () => `
+  //   <sfx-search-box placeholder="${text(
+  //     'Placeholder Title',
+  //     'Placeholder here...'
+  //   )}"></sfx-search-box>
+  // `
+  //   );
