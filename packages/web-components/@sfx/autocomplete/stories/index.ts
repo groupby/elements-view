05d0ff1f1eaeae6a81a6c74b45484d921026b16b
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-21 14:28:48 -0400

    Proofread and update notes in products and autocomplete in prep for PR

--- packages/web-components/@sfx/autocomplete/stories/index.ts
+++ packages/web-components/@sfx/autocomplete/stories/index.ts
@@ -27,20 +27,24 @@ storiesOf('Components|Autocomplete', module)
 
           [SF-X Autocomplete README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/autocomplete "SF-X Autocomplete README").
 
+          \`\`\`html
+          <sfx-autocomplete></sfx-autocomplete>
+          \`\`\`
+
+
           ## Demonstrated in this story:
 
-            * Autocomplete populating with autocomplete data in response to the autocomplete received results event.
-              * To emit the event, visit the Custom Events tab and view the sfx::autocomplete\_received\_results event.
-              * Clicking 'emit' will fire the event, which sfx-autocomplete listens on.
-                * The payload of the event (event detail) should populate on the page.
-            * Autocomplete with an optional title
-              * This is populated by the caption attribute
-                * To update this attribute, visit the Knobs tab and modify the text in the 'Optional Caption' knob and emit the event
-                  * View the DOM and component update with the updated text
-
-            \`\`\`html
-            <sfx-autocomplete></sfx-autocomplete>
-            \`\`\`
+            * The SF-X Autocomplete component populating with autocomplete data in response to the \`sfx::autocomplete_received_results\` event.
+              * To emit the event:
+                1. Visit the **Custom Events** tab and view the \`sfx::autocomplete_received_results\` event.
+                2. Click 'emit'.
+                  * The payload of the event will populate on the page.
+            * The SF-X Autocomplete component populating with an optional title.
+              * This is populated via the caption attribute.
+                * To update the text within the 'caption' attribute:
+                  1. Visit the **Knobs** tab and modify the text in the 'Optional Caption' field
+                  2. Navigate to the **Custom Events** tab and emit the event
+                    * View the DOM and component update with the updated text
             `
       }
     }
--- packages/web-components/@sfx/product/stories/index.ts
+++ packages/web-components/@sfx/product/stories/index.ts
@@ -1,104 +1,51 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
-import { ProductModel } from '../src/index';
 import '../src/index';
-import { getDisplayCode } from '../../../../../.storybook/common';
+import { getDisplayCode, getProducts } from '../../../../../.storybook/common';
 
-const product = {
-  title: 'Best Shoe',
-  price: 39.99,
-  label: 'New Product',
-  promo: '25% off',
-  imageSrc: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
-  imageAlt: 'A spicy red shoe'
-} as ProductModel;
-
-const productWithVariant = {
-  ...product,
-  productUrl: 'https://groupbyinc.com',
-        variants: {
-          type: 'color',
-          items: [
-            { color: '#c00', text: 'Red', product },
-            { color: '#28e', text: 'Blue', product: {
-              imageSrc: 'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80' },
-              imageAlt: 'Sonic blue, gotta go fast'
-            },
-            { color: '#555', text: 'Grey', product: {
-              imageSrc: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80' },
-              imageAlt: 'Magic hovering shoes'
-            },
-          ]
-        }
-      }
-
-const productMarkdownIntro =
-` # SF-X Product Component
+const productMarkdownIntro = ` # SF-X Product Component
 
 [SF-X Product README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/product "SF-X Product README").
 
+\`\`\`html
+<sfx-product></sfx-product>
+\`\`\`
+
 ## Demonstrated in this story:`;
 
-function getProductComponent(product ={}, display = false): string {
+function getProductComponent(product = {}, codeSnippet = false): string {
   const productInfo = text('Product Info', JSON.stringify(product));
 
-  return display === false ? (
-    '<sfx-product\n'
-    + ` product="${productInfo}"\n`
-    + '></sfx-product>'
-  ) : (
-    '<sfx-product>\n'
-    + '</sfx-product>'
-  );
+  return codeSnippet === false
+    ? '<sfx-product\n' + ` product="${productInfo}"\n` + '></sfx-product>'
+    : '<sfx-product>\n' + '</sfx-product>';
 }
 
 storiesOf('Components|Product', module)
   .addDecorator(withKnobs)
-  .add('Default', () => {
-    const product = getProductComponent(productWithVariant)
-    return `
-    ${product}
+  .add(
+    'Default',
+    () => {
+      const productComponent = getProductComponent(getProducts(1)[0]);
+      return `
+    ${productComponent}
     ${getDisplayCode(getProductComponent(null, true))}
-    `
-    } , {
-    notes: {
-      markdown: `
-      ${productMarkdownIntro}
+    `;
+    },
+    {
+      notes: {
+        markdown: `
+        ${productMarkdownIntro}
 
-        * Rendering of hardcoded product data
-          * No styling applied
-          * Hardcoded product data includes variants
-
-        * Demonstrating toggling between variants
-          * To view toggling in story, click on the various coloured squares, and see the product image change.
-
-          \`\`\`html
-          <sfx-product
-          product = "{
-          title: 'Best Shoe',
-          price: 39.99,
-          label: 'New Product',
-          promo: '25% off',
-          imageSrc: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80',
-          imageAlt: 'A spicy red shoe',
-          productUrl: 'https://groupbyinc.com',
-          variants: {
-            type: 'color',
-            items: [
-              { color: '#c00', text: 'Red', product },
-              { color: '#28e', text: 'Blue', product: {
-                imageSrc: 'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80' },
-                imageAlt: 'Sonic blue, gotta go fast'
-              },
-              { color: '#555', text: 'Grey', product: {
-                imageSrc: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&auto=format&fit=crop&h=350&q=80' },
-                imageAlt: 'Magic hovering shoes'
-              },
-            ]
-          }
-        }))}"
-      ></sfx-product>
-      \`\`\`
-    `}
-})
+          * The SF-X Product component rendering with hardcoded product data (randomized).
+            * The product data is being populated via the 'product' attribute (refer to the \`ProductModel\` and \`ProductVariantsModel\` instance for the data format).
+              * To modify the data within the 'product' attribute:
+                1. Visit the **Knobs** tab and modify the data in the 'Product Data' field.
+                  * View the component update with the updated data.
+          * The SF-X Product component updating with variant product data when toggling between variants.
+            * To view toggling in story, click on the various coloured squares and see the product image change.
+        `
+      }
+    }
+  );
