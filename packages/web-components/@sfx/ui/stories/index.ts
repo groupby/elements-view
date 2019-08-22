108ec4003a46110d33f0d76d13c7080309e85cfb
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-22 13:26:48 -0400

    Proof and edit notes in List component

--- packages/web-components/@sfx/ui/stories/index.ts
+++ packages/web-components/@sfx/ui/stories/index.ts
@@ -39,9 +39,9 @@ storiesOf('UI|List', module)
     },
     {
       notes: {
-        markdown: `# SF-X UI Component
+        markdown: `# SF-X List Component
 
-        [SF-X UI README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/ui "SF-X Products README").
+        [SF-X UI README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/ui "SF-X List README")
 
         \`\`\`html
         <sfx-ui
@@ -51,11 +51,17 @@ storiesOf('UI|List', module)
         \`\`\`
 
         ## Demonstrated in this story:
-          * NOTE - this UI component is to be used when a list is required (currently being used by autocomplete)
-          * Rendering a list with hardcoded data and an optional title
-            * To modify the hardcoded data, navigate to the 'Knobs' tab
-              * Modify the data in the in the list items knob and view the data populate on page
-              * Modify the text in the SF-X List Title knob (the caption attribute) and view the data populate on page
+
+          * The SF-X List component rendering a list with data populated via the caption and list attributes.
+            * To modify the hardcoded list data:
+              1. Visit the **Knobs** tab.
+              2. Modify the data in the in the List Items field.
+                * View the component update with the updated data.
+            * To modify the optional title:
+              1. Visit the **Knobs** tab.
+              2. Update the text in the in the SF-X List Title field.
+                * View the DOM (the caption attribute) and the component update with the updated text.
+          * NOTE: The SF-X List component is intended to be used as an UI component in conjunction with other components (see the SF-X Autocomplete component).
         `
       }
     }
