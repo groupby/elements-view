998db72b277506920e7de5b4d5e7393e409b3cec
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-08-23 15:48:02 -0400

    Bind new functions in SAYT

--- packages/web-components/@sfx/sayt/src/sayt.ts
+++ packages/web-components/@sfx/sayt/src/sayt.ts
@@ -53,10 +53,11 @@ export default class Sayt extends LitElement {
     this.showCorrectSayt = this.showCorrectSayt.bind(this);
     this.isCorrectSayt = this.isCorrectSayt.bind(this);
     this.requestSayt = this.requestSayt.bind(this);
+    this.requestSaytAutocompleteTerms = this.requestSaytAutocompleteTerms.bind(this);
+    this.requestSaytProducts = this.requestSaytProducts.bind(this);
     this.processSearchboxInput = this.processSearchboxInput.bind(this);
     this.processSfxSearchboxChange = this.processSfxSearchboxChange.bind(this);
     this.setSearchboxListener = this.setSearchboxListener.bind(this);
-    this.requestSaytAutocompleteTerms = this.requestSaytAutocompleteTerms.bind(this);
   }
 
   /**
