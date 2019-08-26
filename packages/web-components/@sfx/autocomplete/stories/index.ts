f5be23c10f1620e821434ecf0cc05d62455c551d
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-08-26 09:41:01 -0400

    Update autocomplete as per JT's feedback

--- packages/web-components/@sfx/autocomplete/stories/index.ts
+++ packages/web-components/@sfx/autocomplete/stories/index.ts
@@ -2,10 +2,13 @@ import { storiesOf } from '@storybook/html';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { getDisplayCode, autocompleteReceivedResultsEvent } from '../../../../../.storybook/common';
 import '../src/index';
+import { AUTOCOMPLETE_RECEIVED_RESULTS_EVENT } from '../src/index';
 
 function getAutocomplete(): string {
-  const optionalTitle = text('Optional Caption', 'Autocomplete Results');
-  return '<sfx-autocomplete\n' + ` caption="${optionalTitle}"\n` + '></sfx-autocomplete>';
+  const optionalTitle = text('Optional Title', 'Autocomplete Results');
+  return '<sfx-autocomplete\n'
+  + ` caption="${optionalTitle}"\n`
+  + '></sfx-autocomplete>';
 }
 
 storiesOf('Components|Autocomplete', module)
@@ -25,24 +28,24 @@ storiesOf('Components|Autocomplete', module)
         markdown: `
           # SF-X Autocomplete Component
 
-          [SF-X Autocomplete README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/autocomplete "SF-X Autocomplete README").
+          [Package README](https://github.com/groupby/sfx-view/tree/master/packages/web-components/%40sfx/autocomplete "SF-X Autocomplete README").
 
           \`\`\`html
           <sfx-autocomplete></sfx-autocomplete>
           \`\`\`
 
 
-          ## Demonstrated in this story:
+          ## Demonstrated in this story
 
-            * The SF-X Autocomplete component populating with autocomplete data in response to the \`sfx::autocomplete_received_results\` event.
+            * The SF-X Autocomplete component populates with autocomplete data in response to the \`${AUTOCOMPLETE_RECEIVED_RESULTS_EVENT}\` event.
               * To emit the event:
-                1. Visit the **Custom Events** tab and locate the \`sfx::autocomplete_received_results\` event.
-                2. Click 'emit'.
-                  * The payload of the event will populate on the page as a list of autocomplete items.
-            * The SF-X Autocomplete component populating with an optional title.
+                1. Visit the **Custom Events** tab and locate the \`${AUTOCOMPLETE_RECEIVED_RESULTS_EVENT}\` event.
+                2. Click "emit".
+                  * The payload of the event will appear on the page as a list of autocomplete items.
+            * The SF-X Autocomplete component populates with an optional title.
               * The optional title is populated via the 'caption' attribute.
-                * To update the 'caption' attribute text:
-                  1. Visit the **Knobs** tab and modify the text inside the 'Optional Caption' field.
+                * To update the optional title within this story:
+                  1. Visit the **Knobs** tab and modify the text inside the 'Optional Title' field.
                   2. Navigate to the **Custom Events** tab and emit the event.
                     * View the DOM and component update with the updated text.
             `
