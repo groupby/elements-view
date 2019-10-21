0f6e66c30daeafbae3cfba694ae203ec34d37362
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-10-21 17:22:57 -0400

    WIP - fix unit test error - eventTarget is not of type event

--- packages/web-components/@sfx/base/test/unit/base.test.ts
+++ packages/web-components/@sfx/base/test/unit/base.test.ts
@@ -21,4 +21,38 @@ describe('Base Class', () => {
       expect(renderRoot).to.equal(dummyComponent);
     });
   });
+
+  describe('dispatchSfxEvent', () => {
+    xit('should dispatch the provided event from the component', () => {
+      // let dispatchSfxEventStub;
+      const eventName = 'eventName';
+      const payload = { query: 'joffre' }
+      // const dispatchEventStub = stub(window, 'dispatchEvent')
+      const dispatchEventStub = stub(dummyComponent, 'dispatchEvent')
+      const eventToDispatch = new CustomEvent(eventName, { detail: payload, bubbles: true});
+      console.log('typeof eventToDispatch', typeof eventToDispatch)
+      console.log('eventToDispatch', eventToDispatch)
+      // const dispatchSfxEventStub = stub(dummyComponent, 'dispatchSfxEvent');
+      dummyComponent.dispatchSfxEvent(eventName, payload);
+
+      expect(dispatchEventStub).to.have.been.called;
+      // expect(dispatchEventStub).to.have.been.calledWith(eventToDispatch);
+    })
+    it('attempt two should dispatch the provided event from the component', () => {
+      // let dispatchSfxEventStub;
+      stub(window, 'CustomEvent').returns({});
+      const eventName = 'eventName';
+      const payload = { query: 'joffre' }
+      // const dispatchEventStub = stub(window, 'dispatchEvent')
+      const dispatchEventStub = stub(dummyComponent, 'dispatchEvent')
+      // const eventToDispatch = new CustomEvent(eventName, { detail: payload, bubbles: true});
+      // console.log('typeof eventToDispatch', typeof eventToDispatch)
+      // console.log('eventToDispatch', eventToDispatch)
+      // const dispatchSfxEventStub = stub(dummyComponent, 'dispatchSfxEvent');
+      dummyComponent.dispatchSfxEvent(eventName, payload);
+
+      expect(dispatchEventStub).to.have.been.called;
+      // expect(dispatchEventStub).to.have.been.calledWith(eventToDispatch);
+    })
+  })
 });
