846cdda12db30697e0eb92759400618b63a392de
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-10-21 11:18:50 -0400

    Add reusable eventDispatchMethod

--- packages/web-components/@sfx/base/src/base.ts
+++ packages/web-components/@sfx/base/src/base.ts
@@ -6,7 +6,17 @@ import {
  * A base component for all SF-X components to extend. It is based on LitElement.
  */
 export default class Base extends LitElement {
+  constructor() {
+    super();
+    this.dispatchEvent = this.dispatchEvent.bind(this);
+  }
+
   createRenderRoot(): Element {
     return this;
+  }
+
+  dispatchSfxEvent(eventName: string, payload?: any): void {
+    const eventToDispatch = new CustomEvent(eventName, { detail: payload, bubbles: true });
+    this.dispatchEvent(eventToDispatch);
   }
 }
