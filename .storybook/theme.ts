da3fa3cbb836d39cd2d005b74241ab2eefaa49b4
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-10-31 15:30:36 -0400

    Update theme.ts brandTitle

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   
-  brandTitle: 'SFX Components',
+  brandTitle: 'GroupBy Elements Components',
   brandUrl: 'https://www.groupbyinc.com/',
   brandImage: 'https://www.groupbyinc.com/wp-content/uploads/2017/07/Darker-GroupBy-logo.png',
 });
