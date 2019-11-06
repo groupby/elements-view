16ddf6bb86a30ac109f3b23d08fbf49f5c4a67fe
Chantal Desrochers <chantal.desrochers@groupbyinc.com>
2019-11-06 13:34:10 -0500

    Modify loader for karma and storybook to use ts-loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,11 @@
 const webpack = require('../webpack.config.js');
 
+webpack.module.rules.splice(0, 1, {
+  test: /\.tsx?$/,
+  loader: 'ts-loader',
+  exclude: [/node_modules/]
+});
+
 module.exports = async ({ config, mode }) => {
   return {
     ...config,
